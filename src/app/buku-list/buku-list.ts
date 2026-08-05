4e7c201f8dd504febe52855103a6ac5f45871a99
Caprizziest <jasonbintangsetiawan@gmail.com>
2026-08-05 16:28:12 +0800

    modul 13

--- src/app/buku-list/buku-list.ts
+++ src/app/buku-list/buku-list.ts
@@ -1,13 +1,15 @@
 import { Component, signal, computed } from '@angular/core';
 import { Buku, User, Peminjaman } from '../../mock/mock-buku'; 
 import { BukuDetail } from '../buku-detail/buku-detail';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-buku-list',
-  imports: [BukuDetail],
+  imports: [BukuDetail, FormsModule],
   templateUrl: './buku-list.html',
   styleUrl: './buku-list.css',
 })
+
 export class BukuList {
   Buku = Buku;
   User = User;
@@ -20,5 +22,14 @@ export class BukuList {
 
   keteranganPinjam = computed(() => {
     return `Buku ${this.currentBuku().judul} ditulis oleh ${this.currentBuku().pengarang} dipinjam oleh ${this.currentUser().nama} pada tanggal ${this.currentPeminjam().tanggal_pinjam} kembali pada tanggal ${this.currentPeminjam().tanggal_kembali}`;
+  })
+
+  searchTerm = signal("")
+
+  filteredBook = computed(() => {
+    const query = this.searchTerm().toLocaleLowerCase();
+    return this.Buku.filter(buku => {
+      return buku.judul.toLocaleLowerCase().includes(query);
+    })
   })
 }
